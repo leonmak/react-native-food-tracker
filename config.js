1a08269e834deeda2c2edcd459cbd5285dd3d452
leon mak <leonmakansheng@gmail.com>
2017-06-25 02:28:48 +0800

    Add image picker

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Mak on 25/6/17.
+ */
+// TODO: replace with blank values
+const config = {
+  CLOUDINARY: {
+    API_KEY: '376432257264653',
+    API_SECRET: 'mzRuwFMUEYac3jvnoeLX5FrcKKw',
+    CLOUD_NAME: 'leonmak',
+  }
+}
+
+export default config;
